1cb76d73da3574b9cb02c23af11d545ec919ccac
amo22 <amikoamo111@gmail.com>
2013-06-11 22:21:06 +0400

    Update Task_05.h

--- Task_05.h
+++ Task_05.h
@@ -1,10 +1,10 @@
-#define Task_03__GoTo_Next_Step  		Task_03_Step += 1
-#define Task_03__Task_Stop			Task_03_Step  = 0
-#define Task_03__Task_Start			Task_03_Step  = 1
+#define Task_05__GoTo_Next_Step  		Task_05_Step += 1
+#define Task_05__Task_Stop			Task_05_Step  = 0
+#define Task_05__Task_Start			Task_05_Step  = 1
 
 
-extern unsigned char Task_03_Step ;
+extern unsigned char Task_05_Step ;
 
 
 
-void Task_03(void);
+void Task_05(void);
